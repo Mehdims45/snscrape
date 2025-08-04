156f2d148b0699c8391e28d875584345082ecf82
Mehdims45 <mobile4513@gmail.com>
2025-08-04 10:49:56 +0200

    Update __init__.py

--- snscrape/__init__.py
+++ snscrape/__init__.py
@@ -1,12 +1,1 @@
-import importlib
-import os
 
-def _import_modules():
-    for moduleName in sorted(
-        os.path.splitext(filename)[0]
-        for filename in os.listdir(os.path.dirname(__file__))
-        if filename.endswith('.py') and filename not in ('__init__.py',)
-    ):
-        importlib.import_module(f'.{moduleName}', 'snscrape.modules')
-
-_import_modules()
